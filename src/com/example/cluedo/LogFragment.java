4cb1436728f6e6400daf905bd96d5810ad136846
Oskari Lehto <oskari.lehto@gmail.com>
2013-04-17 00:54:26 +0300

    LogFragment now works

--- src/com/example/cluedo/LogFragment.java
+++ src/com/example/cluedo/LogFragment.java
@@ -1,5 +1,7 @@
 package com.example.cluedo;
-import android.app.ListFragment;
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,11 +10,12 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class LogFragment extends ListFragment{
+public class LogFragment extends Fragment{
 		
-	View logView;
 	int currentItem;
+	View logView;
 	ArrayAdapter<String> logAdapter;
 	ListView list;
 	@Override
@@ -22,24 +25,45 @@ public class LogFragment extends ListFragment{
 		//System.out.println(getArguments());
 		//this.logic = (GameLogic) getArguments().getSerializable("GameLogic");
 		
+		
 		logView = inflater.inflate(R.layout.tab_3_layout, null);
 		logAdapter = new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1, ((GameActivity)getActivity()).getLogic().getLogArrayList());
 		
-		list = (ListView)logView.findViewById(android.R.id.list);
-		
-		System.out.println("Tämmönen täälä");
-		System.out.println(list.getOnItemClickListener());
-		list.setOnItemClickListener(new OnItemClickListener() {
-		    public void onItemClick(AdapterView<?> parent, View view, int position,
-		            long id) {
-		        System.out.println("VITTU KYRPÄ PILLU");
-		    	//Toast.makeText(getBaseContext(), "Perse!", Toast.LENGTH_LONG).show();
-		    }
-		});
-		System.out.println("Tämmönen täälä");
-		System.out.println(list.getOnItemClickListener());
-		
+		System.out.println("Aaa");
+		list = (ListView)logView.findViewById(R.id.logListView);
+		System.out.println("BBBB");
+
 	    list.setAdapter(logAdapter);
+	    
+		System.out.println("Tämmönen täälä");
+		System.out.println(list.getOnItemClickListener());
+
+	    list.setOnItemClickListener(new OnItemClickListener() {
+	    	@Override
+	        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	        		System.out.println("Kutsuttiin poistoa");
+	        		currentItem = position;
+		        	new AlertDialog.Builder(logView.getContext())
+		    		.setTitle("Delete log item")
+		    		.setMessage("Are you sure?")
+		    		.setIcon(R.drawable.agent)
+		    		.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+		    			@Override
+		    		    public void onClick(DialogInterface dialog, int whichButton) {
+		    				logAdapter.remove(logAdapter.getItem(currentItem));
+		    		    	((GameActivity)getActivity()).getLogic().removeFromLog(currentItem);
+		                	Toast.makeText(logView.getContext(), "Log item deleted.", Toast.LENGTH_SHORT).show();	
+		    		    }
+		    		})
+		    		 .setNegativeButton(android.R.string.no, null).show();
+	        			
+	        }
+	    });
+		System.out.println("Tämmönen täälä");
+		System.out.println(list.getOnItemClickListener());
+		System.out.println(getActivity().getCurrentFocus());
+		System.out.println(logView.getFocusables(currentItem));
+	    
 		return logView;
 
 		/*
@@ -65,11 +89,5 @@ public class LogFragment extends ListFragment{
 	        }
 	    });
 	    */
-	}
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
 	}
 }
